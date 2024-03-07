36ce245aa7db0ec218abeccafecd40e56db4311d
Nicollas Ryan <nbarretoduarte@gmail.com>
2024-03-07 09:20:36 -0300

    Resolvendo erro

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,15 +4,15 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-  console.error("ERROR", _env.error.format());
+  console.error("⚠️ Invalid environment variables", _env.error.format());
 
-  throw new Error("invalid enviroment variables.");
+  throw new Error("Invalid environment variables.");
 }
 
 export const env = _env.data;
